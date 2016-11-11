fd709efe4e4498d83f174c750c808362ab0a5d16
bradfishman <bradfish@icloud.com>
2016-11-10 18:12:33 -0600

    Lot's of initial scripting

--- script.js
+++ script.js
@@ -1,27 +1,51 @@
-//OUR API KEY FOR OPENWEATHERMAP: 7a3b0e6db8b7743c9fcc94f2caf73771
+var images = {
+    //GENERIC PHOTOS
+    default: "http://mollyirwin.typepad.com/.a/6a00d8349eed6669e20133f413a22f970b-pi",
+    clear: "http://susanstilwell.com/wp-content/uploads/2011/10/dreamstimefree_20823097.jpg",
+    thunderstorm: "http://farmersalmanac.com/wp-content/uploads/2015/06/Thunderstorm-5best.jpg",
+    drizzle: "http://cdn.pcwallart.com/images/rain-cloud-wallpaper-3.jpg",
+    rain: "https://i3.wallpaperscraft.com/image/rain_island_clouds_volume_sky_52002_1920x1080.jpg",
+    snow: "http://www.hdwallpapersact.com/wp-content/uploads/2013/12/snow-falling-in-a-city-wallpapr.jpg",
+    few: "http://mollyirwin.typepad.com/.a/6a00d8349eed6669e20133f413a22f970b-pi",
+    scatteredbroken: "https://coclouds.com/wp-content/uploads/2011/06/illuminated-scattered-clouds-2011-06-21.jpg",
+    overcast: "https://coclouds.com/wp-content/uploads/2011/11/morning-clouds-backlit-spotted-overcast-panoramic-2011-11-28.jpg",
+    //CHICAGO PHOTOS
+    chicago: {
+        clear: "http://cleartheairchicago.com/files/2014/09/2009-09-18_3060x2040_chicago_skyline.jpg",
+        thunderstorm: "https://s-media-cache-ak0.pinimg.com/originals/ba/f2/50/baf250148733333187b1d0c2a902f5ab.jpg",
+        few: "https://c2.staticflickr.com/6/5254/5436125688_966b27402a_b.jpg",
+    }
+};
 
 function getWeather(){
-    // This function gets called when the "Get Weather" button the the HTML page is clicked.
-	// First, we need to retrieve the desired ZIP code from the page:
-    // And now we can build the query to send to the weather API. An API is an interface that 
-	// lets us talk to a different machine - in this case one that provides weather data
-	// If you want to try this code, you MUST get your own API key from openweathermap.org
-	// and put it in place of the <yourkeygoeshere> placeholder !!!!!!!!!!!!!!!!!!!!!!!!!!
-  	var url = "http://api.openweathermap.org/data/2.5/weather?zip=<yourzipcodegoeshere>,US&appid=<yourkeygoeshere>";
-    // Now that we have a nice query, we can send it to the server. Note that this function will terminate
-	// right after the request is sent.
+    //GET FIELD VALUE
+    var zipcode = document.getElementById("zipcode").value;
+    //GENERATE URL AND SEND JSON REQ
+  	var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",US&appid=fc4193a032f2244550b8fb77a65f9c3d";
 	jsonRequest(url);
-    // jsonRequest is a function that handles the actual communication with the server. 
-	// You can take a look at it at the bottom of this file if you like.
 }
 
 function processResponse(wxObject,rawResponse){
-    // We never call this function. It gets "magically" started as soon as we receive the response 
-    // from the weather API. This happens asynchronously.  
-    alert(rawResponse); // This will print out the raw response you got from the server. You should delete this line later.     
+    var newimage;
+    var conditions = wxObject.weather[0].id;
+    if (wxObject.name == "Chicago") {
+        if (/\8\d\d/.test(conditions) == true) {
+            newimage = images.chicago.clear
+        }
+        else {
+            newimage = images.default
+        };
+    }
+    else {
+        newimage = images.default
+    };
+    document.body.style.background = "linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url('" + newimage + "')";
+    document.body.style.backgroundSize = "cover"
+    //alert(wxObject.weather[0].id); // This will print out the raw response you got from the server. You should delete this line later.     
 	// To find out what the response looks like, you can also manually enter a query into your browser. Like this:
     // http://api.openweathermap.org/data/2.5/weather?zip=61801,US&appid=<yourkeygoeshere>	
 	// You should use the wxObject object for your further coding. 
+    
 	}   
 
 function processError(){
