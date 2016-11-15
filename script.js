7120a0e1f5f21123d305c829ed13be9253232575
bradfishman <bradfish@icloud.com>
2016-11-14 22:15:59 -0600

    Small updates

--- script.js
+++ script.js
@@ -14,21 +14,28 @@ var images = {
     chicago: {
         clear: "http://cleartheairchicago.com/files/2014/09/2009-09-18_3060x2040_chicago_skyline.jpg",
         thunderstorm: "https://s-media-cache-ak0.pinimg.com/originals/ba/f2/50/baf250148733333187b1d0c2a902f5ab.jpg",
-        few: "https://c2.staticflickr.com/6/5254/5436125688_966b27402a_b.jpg",
+        clouds: "https://c2.staticflickr.com/6/5254/5436125688_966b27402a_b.jpg",
+        //CHICAGO NIGHT PHOTOS
+        night: {
+            clear: "http://www.richard-seaman.com/Wallpaper/USA/Cities/Chicago/ChicagoAtNightWide.jpg",
+            clouds: "https://s-media-cache-ak0.pinimg.com/originals/2d/f8/26/2df82632870272e3f7858da0977511c5.jpg"
+        }
     },
+    //GENERIC NIGHT PHOTOS
     night: {
         clear: "http://i.imgur.com/HF3Xxg1.jpg",
         clouds: "http://pre03.deviantart.net/cd6a/th/pre/i/2008/239/6/5/cloudy_night_sky_by_ramosburrito.jpg"
     }
-}
+};
+var newimage = "";
 
 function getWeather() {
     //GET FIELD VALUE
     var zipcode = document.getElementById("zipcode").value;
     if (zipcode == "") {
-        zipcode = "61820"
-        document.getElementById("zipcode").value = zipcode
-    };
+        zipcode = "61820";
+        document.getElementById("zipcode").value = zipcode;
+    }
     //GENERATE URL AND SEND JSON REQ
     var url = "http://api.openweathermap.org/data/2.5/weather?zip=" + zipcode + ",US&appid=fc4193a032f2244550b8fb77a65f9c3d";
     jsonRequest(url);
@@ -37,109 +44,120 @@ function getWeather() {
 function processResponse(wxObject, rawResponse) {
     var icon = "";
     var isnight = false;
-    var currenttime = Date.now()/1000;
-    
+    var currenttime = Date.now() / 1000;
+
+    //SETS ICON AND ISNIGHT CONDITION BASED ON TIME OF DAY
     if (currenttime > wxObject.sys.sunrise && currenttime < wxObject.sys.sunset) {
-        icon = "fa-sun-o"
-        isnight = false
+        icon = "fa-sun-o";
+        isnight = false;
     } else {
-        icon = "fa-moon-o"
-        isnight = true
-    };
-    
-    var tempinf = Math.round(wxObject.main.temp * (9/5) - 459.67);
+        icon = "fa-moon-o";
+        isnight = true;
+    }
+
+    //CONVERTS TEMP FROM KELVIN TO F
+    var tempinf = Math.round(wxObject.main.temp * (9 / 5) - 459.67);
     var city = wxObject.name;
-    
+
+    //DISPLAYS THE CITY, TEMP, AND ICON
     document.getElementById("report").innerHTML = "<i id='report' class='fa " + icon + " fa-lg'><span id='reporttext'> " + city + " &ndash; " + tempinf + "&deg;</span></i>";
-    
-    var conddescrip = ""
-    
-    for (i = 0; i < wxObject.weather.length; i++) {
-        if (i > 0) {
-            conddescrip = conddescrip + "&#32; &amp; &#32;" + wxObject.weather[i].description
+
+    //INITIALIZE A STRING TO DESCRIBE THE CONDITIONS
+    var conddescrip = "";
+
+    //LOOP THROUGH THE WEATHER ARRAY ELEMENTS IN API RESPONSE TO COMBINE THE DIFFERENT CONDITIONS INTO ONE STRING
+    for (var i = 0; i < wxObject.weather.length; i++) {
+        if (i == wxObject.weather.length - 1 && i > 0) {
+            conddescrip += "&#32; &amp; &#32;" + wxObject.weather[i].description;
+        } else if (i < wxObject.weather.length && i > 0) {
+            conddescrip += ", &#32;" + wxObject.weather[i].description;
         } else {
-            conddescrip = wxObject.weather[i].description
-        };
-    };
-    
+            conddescrip = wxObject.weather[i].description;
+        }
+    }
+
+    //DISPLAY THE WEATHER CONDITIONS FROM THE LOOP
     document.getElementById("wxconditions").innerHTML = "<p id='wxconditions'>" + conddescrip.toUpperCase() + "</p>";
-    
-    var newimage = "";
+
+    //CONDITIONS IS THE WEATHER CONDITION ID FROM THE API RESPONSE AND LISTED IN THE API DOC
     var conditions = wxObject.weather[0].id;
-    
+
+    //CHANGE THE BACKGROUND IMAGE
+    //TEST TO SEE IF THE LOCATION IS A SPECIFIC CITY FOR THE 30 PT EC ASSIGNMENT
     if (wxObject.name == "Chicago") {
         if (conditions == "800") {
-            newimage = images.chicago.clear
-        } else if (/\8\d\d/.test(conditions) == true) {
-            newimage = images.chicago.few
+            newimage = images.chicago.clear;
+            if (isnight == true) {
+                newimage = images.chicago.night.clear;
+            }
+        } else if (/8\d\d/g.test(conditions) == true) {
+            newimage = images.chicago.clouds;
+            if (isnight == true) {
+                newimage = images.chicago.night.clouds;
+            }
         } else {
-            newimage = images.default
-        };
+            newimage = "";
+        }
     } else if (/2\d\d/g.test(conditions) == true) {
-        newimage = images.thunderstorm
+        newimage = images.thunderstorm;
     } else if (/3\d\d/g.test(conditions) == true) {
-        newimage = images.drizzle
+        newimage = images.drizzle;
     } else if (/5\d\d/g.test(conditions) == true) {
-        newimage = images.rain
+        newimage = images.rain;
     } else if (/6\d\d/g.test(conditions) == true) {
-        newimage = images.snow
+        newimage = images.snow;
     } else if (conditions == "800") {
         if (isnight == true) {
-            newimage = images.night.clear
+            newimage = images.night.clear;
         } else {
-            newimage = images.clear
-        };
+            newimage = images.clear;
+        }
     } else if (conditions == "801") {
-        newimage = images.few
+        newimage = images.few;
     } else if (conditions == "802" || conditions == "803") {
-        newimage = images.scatteredbroken
+        newimage = images.scatteredbroken;
     } else if (conditions == "804") {
-        newimage = images.overcast
+        newimage = images.overcast;
     } else if (conditions == "701" || conditions == "721" || conditions == "741") {
-        newimage = images.mistfoghaze
+        newimage = images.mistfoghaze;
     } else {
-        newimage = ""
-    };
-    
+        newimage = "";
+    }
+
+    //APPLY NEW IMAGE TO THE BACKGROUND OF THE PAGE
     document.body.style.background = "linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url('" + newimage + "')";
+    //SET CSS SO BACKGROUND COVERS THE PAGE
     document.body.style.backgroundSize = "cover";
+    //SET THE BACKGROUND POSITION
     document.body.style.backgroundPosition = "center center";
+    //IF THE BACKGROUND IS ONE THAT LOOKS BETTER IN THE CENTER BOTTOM POSITION, SET THAT
     if (newimage == images.clear) {
         document.body.style.backgroundPosition = "center bottom";
-    };
-    //alert(conditions); // This will print out the raw response you got from the server. You should delete this line later.     
-        // To find out what the response looks like, you can also manually enter a query into your browser. Like this:
-        // http://api.openweathermap.org/data/2.5/weather?zip=61801,US&appid=<yourkeygoeshere>	
-        // You should use the wxObject object for your further coding. 
+    }
+    //alert(conditions);
 }
 
 function processError() {
-    // We never call this function either. It gets started if we encounter an error while retrieving info  
-    // from the weather API.   
-    // You can use this function to display an error message and image if something goes wrong
-    alert("There was an error while retrieving your weather. This could be due to too many requests.");
+    alert("There was an error while retrieving your weather. This could be due to too many requests or an improper ZIP code.");
+    //BLANK OUT THE BACKGROUND IMAGE (LOOK AT THE ASSIGNMENT WE MIGHT NEED TO CHANGE THE IMAGE TO AN ERROR IMAGE BASED ON THE RUBRIC)
     newimage = "";
     document.body.style.background = "linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url('" + newimage + "')";
     document.body.style.backgroundSize = "cover";
+    //REMOVE THE CONDITION DESCRIPTION
+    document.getElementById("report").innerHTML = "";
+    document.getElementById("wxconditions").innerHTML = "";
 }
 
 
 function showHint() {
+    //SHOWS TIP TO USER IF QUESTION MARK IS CLICKED
     alert("Important information to receive the 30 points extra credit: \n\nEnter one of the following cities/ZIP codes to see unique images for certain cities:\n\nChicago (e.g. 60609), Los Angeles (e.g. 90xxx)\n");
 }
 
-
-
-
-
+//DO NOT EDIT BELOW
 
 function jsonRequest(url) {
     var con = new XMLHttpRequest();
-    // The following is a function within an event handler within an object. 
-    // We have not covered this in class, but basically this nested function
-    // will get called whenever the "state" of the connection changes - usually 
-    // that means that we either got a valid response or an error message. 
-    // Sometimes a connection will time out and then this never gets called.
     con.onreadystatechange = function () {
         if (con.readyState === XMLHttpRequest.DONE) {
             // A connection's state can change multiple times, so we need to check whether 
@@ -154,7 +172,7 @@ function jsonRequest(url) {
             }
         }
     };
-    // This opens the connection to teh server and sends the actual request:
+    // This opens the connection to the server and sends the actual request:
     con.open("GET", url, true);
     con.send();
 }
